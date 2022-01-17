1038cb711801054eb430f0bdc36f95a9488388de
Password-Classified <85027459+Password-Classified@users.noreply.github.com>
2022-01-17 19:52:25 +0000

    👌 IMPROVE: finish initial design of computer-assistant

--- README.md
+++ README.md
@@ -11,20 +11,21 @@ Within this repository, we have many different assistants. Here is a description
 <br/>
 <br/>
 
-#### **Note: Only Computer Assistant works presently.**
+#### **Note: Only Computer and Helpful Assistant works presently.**
 
 ------------------
 
-## [Basic Assistant](https://github.com/Password-Classified/Voice-Assistants/tree/master/basic-assistant)
+## [Computer Assistant](https://github.com/Password-Classified/Voice-Assistants/tree/master/computer-assistant)
+
+Inspired by the Star Trek computer, this is my assistant. Refer to as "computer".
 
 <br/>
+
+## [Basic Assistant](https://github.com/Password-Classified/Voice-Assistants/tree/master/basic-assistant)
 
 After following a NeuralNine YouTube tutorial, I created this assistant that follows basic commands, but is quite slow.
 
 ## [Helpful Assistant](https://github.com/Password-Classified/Voice-Assistants/tree/master/helpful-assistant)
 
-<br/>
-
 A helpful assistant.
 
-<br/>
--- computer-assistant/main.py
+++ computer-assistant/main.py
@@ -1,130 +1,189 @@
 import json
 import random
+import time
 from difflib import SequenceMatcher
+from typing import Callable, Tuple, Union
 
+import speech_recognition as sr
 import text_to_speech as tts
-from speech_recognition import Microphone, Recognizer, UnknownValueError
 
 
-class ComputerAssistant(object):
-    """Base class for all Assistants."""
+# ==================== CONSTANTS ==================== #
 
-    def __init__(self, intents: str, similarity_threshold: float = 0.5) -> None:
-        """Create an Assistant object."""
-        with open(intents) as file:
-            self._intents = file.read()
-        self._max_tag = "nothing"
-        self._blank_audios = ""
-        self._similarity_threshold = similarity_threshold
-        self.notUnderstand_responses = [
-            "I'm sorry, I did not understand that",
-            "Please would you repeat that",
-            "That may not be in my database",
-            "That may not be in my vocabulary",
-            "I could not understand that"
-        ]
+PREFIXES = ("computer", "peter") # since it mistakes computer for peter sometimes (that may just be because I have a small laptop mic)
 
-        self.recognizer = Recognizer()
+# =================================================== #
 
-    def _check_compatability(self, sequence: str, to_compare: str) -> float:
-        """Check compatability ratio."""
-        self._matcher = SequenceMatcher(a=sequence, b=to_compare)
-        return self._matcher.ratio()
+def recognize(recognizer) -> str:
+	try:
+		with sr.Microphone() as mic:
+			print("[ Functional ]\tAdjusting...", end="")
+			recognizer.adjust_for_ambient_noise(mic, duration=0.5)
+			print("\r[ Functional ]\tListening...")
+			audio = recognizer.listen(mic)
 
-    def _parse_intents(self) -> dict:
-        """Parse the intents into a dictionary."""
-        # Just as a separate method in case I need to add more parsing and editing
-        self._intents = json.loads(self._intents)
-        for tag in self._intents:
-            for index, value in enumerate(self._intents[tag]["patterns"]):
-                self._intents[tag]["patterns"][index] = "computer " + \
-                    value.lower()
+			voice_input = recognizer.recognize_google(audio).lower()
+			print(voice_input)
 
-        return self._intents
+	except sr.UnknownValueError:
+		print("Reinitializing...")
+		recognizer = sr.Recognizer()  # Reinitialize
+		try:
+			recognize(recognizer)
+		except RecursionError:
+			tts.speak("You have not asked me a question in a while. Shutting down.")
+			exit()
+	
+	return voice_input
 
-    def _recognize(self) -> None:
-        """Recognize data from input."""
-        try:
-            with Microphone() as mic:
-                print("Adjusting...")
-                self.recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-                print("Listening...")
-                audio = self.recognizer.listen(mic)
 
-                self.input = self.recognizer.recognize_google(audio).lower()
+class Responses(object):
+	@staticmethod
+	def end() -> None:
+		tts.speak(random.choice([
+			"Bye.",
+			"See you later.",
+			"Ok, shutting down.",
+			"Goodbye"
+		]))
 
-        except UnknownValueError:
-            print("Reinitializing...")
-            self.recognizer = Recognizer()  # Reinitialize
-            try:
-                self._recognize()
-            except RecursionError:
-                tts.speak(
-                    "You have not asked me a question in a while. Shutting down.")
-                exit()
+		exit()
 
-    def _get_probs(self, pattern: str) -> str:
-        """Get the probability of it being any certain string."""
-        self._max_tag = "notUnderstand"
+	@staticmethod
+	def isTime() -> None:
+		tts.speak(random.choice([
+			"The time is",
+			"It is",
+			"It's"
+		]) + time.strftime(" %I %M %p")) # starting whitespace is needed to stop time blurring into the prefix phrases in the lines above
 
-        print(pattern)
-        self._probabilities = {}
-        for key in self._intents:
-            self._probabilities[key] = [
-                self._check_compatability(item, pattern)
-                for item in self._intents[key]["patterns"]
-            ]
+	@staticmethod
+	def addTodo(recognizer, set_memory: Callable, memory) -> None:
+		tts.speak(random.choice([
+			"What should be added?",
+			"What should I add?"
+		]))
 
-    def _get_max_prob(self) -> str:
-        """Get the maximum probability and its tag."""
-        self._max_tag = "notUnderstand"
-        self._max_value = self._similarity_threshold
-        for key in self._probabilities:
-            for item in self._probabilities[key]:
-                if item > self._max_value:
-                    self._max_value = item
-                    self._max_tag = key
-        print(self._max_tag)
-        return self._max_tag
+		voice_input = recognize(recognizer)
+		memory["storage"]["todo"].append(voice_input)
+		set_memory()
+		
+		tts.speak(f"Added {voice_input} to your to do list.")
 
-    def act_functions(self):
-        if self._max_tag == "exit":
-            tts.speak(random.choice(
-                [
-                    "Goodbye.",
-                    "Shutting down.",
-                    "Program terminated.",
-                    "See ya later.",
-                    "Cheerio."
-                ]
-            ))
-            exit()
+	@staticmethod
+	def endTodo(recognizer, set_memory: Callable, memory) -> None:
+		tts.speak(random.choice([
+			"What should be removed?",
+			"What should I delete?",
+			"Which item should I delete?"
+		]))
 
-    def act_strings(self):
-        tts.speak(random.choice(
-            self._intents[self._max_tag]["responses"]))
+		voice_input = recognize(recognizer)
+		if voice_input in memory["storage"]["todo"]:
+			memory["storage"]["todo"].remove(voice_input)
+			return None
+		
+		ratios = [SequenceMatcher(a=a, b=voice_input).ratio() for a in memory["storage"]["todo"]]
+		most_likely = memory["storage"]["todo"][ratios.index(max(ratios))]
 
-    def act(self):
-        if self._max_tag == "notUnderstand":
-            tts.speak(random.choice(self.notUnderstand_responses))
-        elif self._intents[self._max_tag]["responses"] != []:
-            self.act_strings()
-        else:
-            self.act_functions()
+		tts.speak(random.choice([
+			"Did you mean",
+		]) + f" {most_likely}")
 
-    def run(self) -> None:
-        self._parse_intents()
-        print("Ready to help!")
-        while True:
-            self._recognize()
-            print(self.input)
-            if "computer" in list(self.input.split(" ")):
-                self._get_probs(self.input)
-                self._get_max_prob()
-                self.act()
+		voice_input = recognize(recognizer)
+		if SequenceMatcher(a=voice_input, b="yes").ratio() > 0.6:
+			memory["storage"]["todo"].remove(most_likely)
+		else:
+			tts.speak("Cancelling remove.")
+		
+		set_memory()
+
+
+
+class Assistant(object):
+	def __init__(self) -> None:
+		self.recognizer = sr.Recognizer()
+
+		with open("./intents.json", "r") as file:
+			self.intents = json.load(file)
+		
+		with open("./memory.json", "r") as file:
+			self.memory = json.load(file)
+	
+	def set_memory(self) -> None:
+		with open("./memory.json", "r") as file:
+			json.dump(self.memory, file)
+
+	@staticmethod
+	def _compare(a: str, b: str) -> float:
+		return SequenceMatcher(a=a, b=b).ratio()
+
+	def _choose(self, speech) -> Union[str, Callable]:
+		choice = ("unknown", 0)
+
+		for k, v in self.intents["intents"].items():
+
+			for pattern in v["patterns"]:
+				ratio = self._compare(pattern, speech)
+				if ratio > choice[1]:
+					choice = (k, ratio)
+
+		return choice[0]
+
+	def _respond(self, choice) -> None:
+		if choice == "unknown":
+			tts.speak(random.choice(self.intents["unknown_responses"]))
+
+		elif self.intents["intents"][choice]["isFunc"]:
+			args = [getattr(self, arg) for arg in self.intents["intents"][choice]["args"]]
+
+			getattr(Responses, choice)(*args)
+
+		else:
+			tts.speak(random.choice(self.intents["intents"][choice]["responses"]))
+
+	def _recognize(self) -> str:
+		try:
+			with sr.Microphone() as mic:
+				print("[ Command ]\tAdjusting...", end="")
+				self.recognizer.adjust_for_ambient_noise(mic, duration=0.5)
+				print("\r[ Command ]\tListening...")
+				audio = self.recognizer.listen(mic)
+
+				self.input = self.recognizer.recognize_google(audio).lower()
+				print(self.input)
+
+		except sr.UnknownValueError:
+			print("[ Command ]\tReinitializing...")
+			self.recognizer = sr.Recognizer()  # Reinitialize
+			try:
+				self._recognize()
+			except RecursionError:
+				tts.speak("You have not asked me a question in a while. Shutting down.")
+				exit()
+
+		return self.input
+	
+	def _process_prefixes(self, voice_input: str, prefixes: Tuple[str]) -> str:
+		for prefix in prefixes:
+			if voice_input.startswith(prefix):
+				voice_input = voice_input.removeprefix(prefix)
+
+		return voice_input
+
+	def assist(self) -> None:
+		while True:
+			voice_input = self._recognize()
+			if voice_input.strip().startswith(PREFIXES):
+				
+				choice = self._choose(voice_input.strip())
+				self._respond(choice)
+
+
+def main():
+	assistant = Assistant()
+	assistant.assist()
 
 
 if __name__ == "__main__":
-    print()
-    assistant = ComputerAssistant("./Intents/intents.json", 0.5)
-    assistant.run()
+	main()
